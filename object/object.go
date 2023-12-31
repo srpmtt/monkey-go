2f35d1d5adeca16af25689f24ae9268fe066b031
srpmtt <m.serpi@outlook.com>
2023-12-31 19:12:57 +0100

    object sustem

---
+++ object/object.go
@@ -0,0 +1,48 @@
+package object
+
+import "fmt"
+
+const (
+	INTEGER_OBJ = "INTEGER"
+	FLOAT_OBJ   = "FLOAT"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
+)
+
+// OBJECT
+type ObjectType string
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+// INTEGER
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+
+// FLOAT
+type Float struct {
+	Value float64
+}
+
+func (f *Float) Type() ObjectType { return FLOAT_OBJ }
+func (f *Float) Inspect() string  { return fmt.Sprintf("%.1f", f.Value) }
+
+// BOOLEAN
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+
+// NULL
+type Null struct{}
+
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) Inspect() string  { return "null" }
