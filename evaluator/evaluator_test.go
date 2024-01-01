8be2054783af0c0f83d80b04d737586100c98a53
srpmtt <m.serpi@outlook.com>
2024-01-01 19:05:41 +0100

    test closures

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -199,6 +199,15 @@ func TestFunctionApplication(t *testing.T) {
 	}
 }
 
+func TestClosures(t *testing.T) {
+	input := `let newAdder = fn(x) {
+              fn(y) { x + y };
+            };
+            let addTwo = newAdder(2);
+            addTwo(2);`
+	testIntegerObject(t, testEval(input), 4)
+}
+
 func TestReturnStatements(t *testing.T) {
 	tests := []struct {
 		input    string
